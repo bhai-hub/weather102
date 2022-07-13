ffeadb4b03186ce3cd12e4b1b222a297661d2914
Bhairav K sharma <51754072+bhai-hub@users.noreply.github.com>
2022-07-13 22:59:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// 'http://api.weatherapi.com/v1/current.json?key=3d8c3846d674495ba38141908210511&q=' + city + '&aqi=no'
+
+
+let res;
+    
+
+function getWeather(){
+    fetch('https://api.weatherapi.com/v1/current.json?key=3d8c3846d674495ba38141908210511&q=' + $("#ct").val()+ '&aqi=no')
+  .then(response => response.json())
+  .then(data => {
+    res = data;
+    $("#cityName").html(res.location.name)
+    $("#temp_c").html(" "+ res.current.temp_c + "°C")
+    $("#condition").html(" "+ res.current.condition.text)
+    $("#image").attr("src", res.current.condition.icon)
+  });
+}
+
